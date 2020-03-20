02884210a979c020e1967ffe5792d29aa9ec6a40
Tielsen <48878423+Tielsen@users.noreply.github.com>
2020-03-20 08:47:56 -0300

    Delete config.py

--- IMOBI-BackEnd/config.py
+++
@@ -1,4 +0,0 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql:admin:imobdb10@imob1.cunjxbhpw7la.us-east-1.rds.amazonaws.com:3306/imob1'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "TRATORES FEIOS"
